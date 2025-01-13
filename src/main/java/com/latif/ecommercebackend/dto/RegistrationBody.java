b9248d9449f0863db861d592039ced9233b25e1f
latifzouliga <zuuliga@gmail.com>
2025-01-12 19:01:41 -0500

    added openApi and validation

--- src/main/java/com/latif/ecommercebackend/dto/RegistrationBody.java
+++ src/main/java/com/latif/ecommercebackend/dto/RegistrationBody.java
@@ -14,35 +14,63 @@ import lombok.ToString;
 public record RegistrationBody(
 
         @Schema(example = "John")
-        @NotNull
-        @NotBlank
-        @Size(min = 2, max = 255)
+        @NotNull(message = "First name is required")
+        @NotBlank(message = "First name cannot be blank")
+        @Size(
+                min = 2,
+                max = 255,
+                message = "First name must be between 2 and 255 characters"
+        )
         String firstName,
 
         @Schema(example = "Doe")
-        @NotNull
-        @NotBlank
-        @Size(min = 2, max = 255)
+        @NotNull(message = "Last name is required")
+        @NotBlank(message = "Last name cannot be blank")
+        @Size(
+                min = 2,
+                max = 255,
+                message = "Last name must be between 2 and 255 characters"
+        )
         String lastName,
 
         @Schema(example = "johndoe")
-        @NotNull
-        @NotBlank
-        @Size(min = 2, max = 255)
+        @NotNull(message = "Username is required")
+        @NotBlank(message = "Username cannot be blank")
+        @Size(
+                min = 2,
+                max = 255,
+                message = "Username must be between 2 and 255 characters"
+        )
         String username,
 
         @Schema(example = "example@email.com")
-        @Email
-        @NotNull
-        @NotBlank
-        @Size(min = 6, max = 36)
+        @Email(
+                regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$",
+                message = "Invalid email format"
+        )
+        @NotNull(message = "Email is required")
+        @NotBlank(message = "Email cannot be blank")
+        @Size(
+                min = 6,
+                max = 36,
+                message = "Email must be between 6 and 36 characters"
+        )
         String email,
 
         @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
-        @NotNull
-        @NotBlank
-        @Size(min = 6, max = 255)
-        @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{6,}$")
+        @NotNull(message = "Password is required")
+        @NotBlank(message = "Password cannot be blank")
+        @Size(
+                min = 6,
+                max = 255,
+                message = "Password must be between 6 and 255 characters")
+        @Pattern(
+                regexp = "(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@#$%^&+=]).*",
+                message = """ 
+                        Password must contain at least one lowercase letter, one uppercase letter, 
+                        one digit, and one special character.
+                        """
+        )
         String password
 ) {
 }
